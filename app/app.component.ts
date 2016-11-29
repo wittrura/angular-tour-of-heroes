4327e3cbc8d25c61a5d585b961a52cb1d7361e16
Ryan Wittrup <ryan.wittrup@gmail.com>
2016-11-28 21:57:43 -0500

    Adds navigation between dashboard and heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>{{title}}</h1>
-  <my-heroes></my-heroes>
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `
 })
 
