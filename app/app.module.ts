1546aada189e586323c07f1d1f995279332a2af4
Ryan Wittrup <ryan.wittrup@gmail.com>
2016-11-28 22:31:55 -0500

    Creates new getHero method to be used for details view

--- app/app.module.ts
+++ app/app.module.ts
@@ -27,6 +27,10 @@ import { HeroService } from './hero.service';
         path: '',
         redirectTo: '/dashboard',
         pathMatch: 'full'
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
       }
     ])
   ],
