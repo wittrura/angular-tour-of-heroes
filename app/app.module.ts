4cd3ecb30c7af1d46208b22db6797d6295a46f18
Ryan Wittrup <ryan.wittrup@gmail.com>
2016-11-29 21:36:37 -0500

    Simulates API call with in-memory service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -10,12 +16,14 @@ import { DashboardComponent } from './dashboard.component';
 
 import { HeroService } from './hero.service';
 
-import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'app/heroes'; // URL to web api
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http) { }
+
+// where to use API for database endpoint
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
   }
 
   getHero(id: number): Promise<Hero> {
@@ -14,10 +25,24 @@ export class HeroService {
                  .then(heroes => heroes.find(hero => hero.id === id));
   }
 
+  update(hero: Hero): Promise<Hero[]> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(()=> hero)
+      .catch(this.handleError);
+  }
+
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise<Hero[]>(resolve =>
       setTimeout(resolve, 2000)) // delay 2 seconds
       .then(() => this.getHeroes());
   }
-  // where to use API for database endpoint
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
