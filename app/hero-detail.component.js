385e63b58a9ed83e537233f4e048b1c551bc7904
Ryan Wittrup <ryan.wittrup@gmail.com>
2016-11-29 21:07:08 -0500

    Adds styling and moves CSS to separate files

--- app/hero-detail.component.js
+++ app/hero-detail.component.js
@@ -37,7 +37,8 @@ var HeroDetailComponent = (function () {
         core_1.Component({
             moduleId: module.id,
             selector: 'my-hero-detail',
-            templateUrl: 'hero-detail.component.html'
+            templateUrl: 'hero-detail.component.html',
+            styleUrls: ['hero-detail.component.css']
         }), 
         __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.ActivatedRoute, common_1.Location])
     ], HeroDetailComponent);
